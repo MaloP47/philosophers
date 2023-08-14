3693e748a528fc025a3be8df5587c36f23b5cc5c
root <root@Asus-MaloP47>
2023-08-14 14:32:05 +0200

    Seed for philospohers

---
+++ philo/includes/philosophers.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpeulet <mpeulet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/14 14:03:58 by mpeulet           #+#    #+#             */
+/*   Updated: 2023/08/14 14:19:33 by mpeulet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <strings.h>
+# include <sys/time.h>
+# include <pthread.h>
+
+#endif
